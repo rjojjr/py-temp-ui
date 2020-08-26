284a52a1b4d13d8d663d2d48917828f570ceb4bf
Robert Kirchner JR <admin@kirchnerbusinesssolutions.com>
2020-08-25 21:15:39 -0500

    init - Add footer and loading modal - RK

---
+++ src/components/global/footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+
+    return(
+        <>
+            <footer>
+                <a className={"lightText"} href={"http://kirchnerbusinesssolutions.com"}>2020 Kirchner Business Solutions</a>
+            </footer>
+        </>
+    );
+
+}
+
+export default Footer
