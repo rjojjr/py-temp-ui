1ef91544de08d8bbd43537c414cf84e69165477c
Robert Kirchner JR <admin@kirchnerbusinesssolutions.com>
2020-08-25 21:36:24 -0500

    init - Add useReducer hook - RK

--- src/App.js
+++ src/App.js
@@ -2,7 +2,34 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const initialStatuses = [];
+
+const basicReducer = (state, action) => {
+  switch (action.type) {
+    case 'UPDATE':
+      return state.map(status => {
+        if (status.id === action.id) {
+          return { ...status, now: action.now, day: action.day , week: action.week};
+        } else {
+          return status;
+        }
+      });
+    default:
+      return state;
+  }
+};
+
 function App() {
+
+  const [statuses, dispatch] = React.useReducer(
+      basicReducer,
+      initialStatuses
+  );
+
+  const handleChange = todo => {
+    dispatch({ type: 'DO_TODO', id: todo.id });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
