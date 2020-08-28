dde1cd4da9a2eeec0d8f55d10af4d3d90b2c72df
Robert Kirchner JR <admin@kirchnerbusinesssolutions.com>
2020-08-28 16:49:33 -0500

    story-11 - Call new summary endpoint - RK

--- src/services/axios-service.js
+++ src/services/axios-service.js
@@ -1,13 +1,12 @@
 import axios from "axios";
 
-export const getSummary = (url, room) => {
+export const getSummary = () => {
 
-    console.log("here")
     const options = {
         headers: {
             'Content-Type': 'application/json'
         }
     };
 
-    return axios.get(`http://${url}/summary/${room}`);
+    return axios.get(`http://192.168.1.169:8080/summary`);
 };
--- src/services/summary-service.js
+++ src/services/summary-service.js
@@ -3,25 +3,25 @@ import { networkErrorMsg, loadingMsg, doneLoadingMsg, sensors } from "../constan
 
 export async function getAllStatuses(handleMsg, handleStatus) {
     handleMsg(loadingMsg);
-    for (var sensor of sensors){
-        await getStatus(sensor, handleMsg, handleStatus);
-    }
-    console.log("done")
+    await getStatus(handleMsg, handleStatus);
+
     handleMsg(doneLoadingMsg);
 }
 
-export async function getStatus(sensor, handleMsg, handleStatus) {
+export async function getStatus(handleMsg, handleStatus) {
     try {
-        const apiResponse = await getSummary(sensor.url, sensor.room);
+        const apiResponse = await getSummary();
+        console.log("done", apiResponse)
         if(apiResponse.status === 200){
-            console.log('resp: ', apiResponse)
-            const status = {
-                id: sensor.room,
-                now: apiResponse.data.now,
-                day: apiResponse.data.day,
-                week: apiResponse.data.week
-            };
-            handleStatus(status);
+            for (var index in apiResponse.data) {
+                const statusJson = {
+                    id: apiResponse.data[index].sensor,
+                    now: apiResponse.data[index].now,
+                    day: apiResponse.data[index].day,
+                    week: apiResponse.data[index].week
+                };
+                handleStatus(statusJson);
+            }
         } else {
             handleMsg(networkErrorMsg);
         }
