7533dcd864bd13ba9e1b421620f76f594b50d9a7
Robert Kirchner JR <admin@kirchnerbusinesssolutions.com>
2020-08-31 15:09:44 -0500

    story-22 - Change loading model color - RK

--- src/components/global/LoadingView.js
+++ src/components/global/LoadingView.js
@@ -23,7 +23,7 @@ const LoadingView = ({ isLoading }) => {
       <ReactLoading
         className="loadingBubbles"
         type={'spinningBubbles'}
-        color={'#1976D2'}
+        color={'#343a40'}
         height={'10rem'}
         width={'10rem'}
       />
