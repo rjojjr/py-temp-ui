6dfc86288b6448dde82b373b94be03035e5b4e4f
Robert Kirchner JR <admin@kirchnerbusinesssolutions.com>
2020-09-01 10:41:02 -0500

    story-14 - Assert on refresh button click - RK

--- src/components/pages/home.test.js
+++ src/components/pages/home.test.js
@@ -11,29 +11,29 @@ describe('home', () => {
 
     describe('render', () => {
 
+        const success = {
+            status: 200,
+            data: [
+                {
+                    sensor: "office",
+                    lastUpdate: "now",
+                    now: [70, 70],
+                    day: [70, 70],
+                    week: [70, 70]
+                }
+            ]
+
+        }
+
+        const status = {
+            id: "office",
+            lastUpdate: "now",
+            now: [70, 70],
+            day: [70, 70],
+            week: [70, 70]
+        }
+
         it('properly', async () => {
-
-            const success = {
-                status: 200,
-                data: [
-                    {
-                        sensor: "office",
-                        lastUpdate: "now",
-                        now: [70, 70],
-                        day: [70, 70],
-                        week: [70, 70]
-                    }
-                ]
-
-            }
-
-            const status = {
-                id: "office",
-                lastUpdate: "now",
-                now: [70, 70],
-                day: [70, 70],
-                week: [70, 70]
-            }
 
             const mock = jest.spyOn(axiosService, "getSummary");
             mock.mockResolvedValue(success);
@@ -49,6 +49,33 @@ describe('home', () => {
             expect(container.querySelector("div.homePage")).toBeInTheDocument();
             expect(container.querySelector("footer")).toBeInTheDocument();
             expect(container.querySelector("table")).toBeInTheDocument();
+            expect(container.querySelector("button")).toBeInTheDocument();
+        });
+
+        it('re-render when pressing refresh button', async () => {
+
+            const mock = jest.spyOn(axiosService, "getSummary");
+            mock.mockResolvedValue(success);
+
+            const {container, findByText} = render(
+                <App/>
+            );
+
+            expect(container.querySelector("div.loader")).toBeInTheDocument();
+
+            const label = await findByText("Py Temp");
+
+            const button = container.querySelector("button");
+
+            fireEvent.click(button)
+
+            expect(container.querySelector("div.loader")).toBeInTheDocument();
+            await findByText("Py Temp");
+            expect(container.querySelector("div#homeNav")).toBeInTheDocument();
+            expect(container.querySelector("div.homePage")).toBeInTheDocument();
+            expect(container.querySelector("footer")).toBeInTheDocument();
+            expect(container.querySelector("table")).toBeInTheDocument();
+            expect(container.querySelector("button")).toBeInTheDocument();
         });
 
     })
