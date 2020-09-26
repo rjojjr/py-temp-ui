407bcd34e76cac4d8432f9dca9905cff4dbf3b31
Robert Kirchner JR <admin@kirchnerbusinesssolutions.com>
2020-09-26 16:51:54 -0500

    story-23 - Add rough chart loader - RK

---
+++ src/components/loaders/chart-loader.js
@@ -0,0 +1,28 @@
+import React, {useContext, useEffect} from "react";
+import RootContext from "../context/root-context";
+import {getAllStatuses} from "../../services/summary-service";
+import LoadingView from "../global/LoadingView";
+import Home from "../pages/home";
+
+
+const ChartLoader = () => {
+
+
+    const state = useContext(RootContext)
+
+    useEffect( () => {
+        getAllStatuses( state.handleMsgChange,  state.handleStatusChange);
+    }, [getAllStatuses]);
+
+
+
+    return(
+
+        <div className={"admin-container"}>
+            <LoadingView isLoading={state.isLoading} message={"Loading..."}/>
+            {!state.isLoading && (
+                <Home/>
+            )}
+        </div>
+    );
+}
