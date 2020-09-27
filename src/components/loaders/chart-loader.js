e9ca37e48fc47ba532c76718881e6134cdabbd9f
Robert Kirchner JR <admin@kirchnerbusinesssolutions.com>
2020-09-27 12:12:28 -0500

    story-23 - Add date service - RK

--- src/components/loaders/chart-loader.js
+++ src/components/loaders/chart-loader.js
@@ -3,22 +3,14 @@ import RootContext from "../context/root-context";
 import LoadingView from "../global/LoadingView";
 import Home from "../pages/home";
 import {getChart} from "../../services/chart-service";
+import {today} from "../../services/date-service";
 
 
 const ChartLoader = () => {
 
 
     const state = useContext(RootContext)
-
-    const today = () => {
-        const today = new Date();
-        const dd = String(today.getDate()).padStart(2, '0');
-        const mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-        const yyyy = today.getFullYear();
-
-        return yyyy + '-' + mm + '-' + dd;
-    }
-
+    
     useEffect( () => {
         getChart( state.handleMsgChange,  state.handleChartChange, 'temp', today(), today());
     }, [getChart]);
