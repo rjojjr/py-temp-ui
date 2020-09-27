7d7d114dc9846f93d3e927b854114e3ff6f38419
Robert Kirchner JR <admin@kirchnerbusinesssolutions.com>
2020-09-27 12:05:25 -0500

    story-23 - Wire up chart useEffect hook - RK

--- src/components/loaders/chart-loader.js
+++ src/components/loaders/chart-loader.js
@@ -1,8 +1,8 @@
 import React, {useContext, useEffect} from "react";
 import RootContext from "../context/root-context";
-import {getAllStatuses} from "../../services/summary-service";
 import LoadingView from "../global/LoadingView";
 import Home from "../pages/home";
+import {getChart} from "../../services/chart-service";
 
 
 const ChartLoader = () => {
@@ -10,9 +10,18 @@ const ChartLoader = () => {
 
     const state = useContext(RootContext)
 
+    const today = () => {
+        const today = new Date();
+        const dd = String(today.getDate()).padStart(2, '0');
+        const mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+        const yyyy = today.getFullYear();
+
+        return yyyy + '-' + mm + '-' + dd;
+    }
+
     useEffect( () => {
-        getAllStatuses( state.handleMsgChange,  state.handleStatusChange);
-    }, [getAllStatuses]);
+        getChart( state.handleMsgChange,  state.handleChartChange, 'temp', today(), today());
+    }, [getChart]);
 
 
 
